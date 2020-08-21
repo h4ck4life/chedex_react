be7492432d5cdcd7b9f12707bdacd9e8f3f3ab98
Alif <alifaziz@gmail.com>
2020-08-21 22:55:00 +0800

    Working version of search bar with mock data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -7,34 +7,43 @@ import SearchPage from "./pages/SearchPage";
 
 import "./styles.css";
 
-const App = () => (
-  <Router>
-    <Container className="p-3">
-      <Navbar bg="light" expand="lg">
-        <Link className="navbar-brand" to="/">
-          <h3>Chedex</h3>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-            <Nav.Link
-              target="_blank"
-              href="https://github.com/h4ck4life/chedex_react"
-            >
-              Github
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Switch>
-        <Route path="/about">{AboutPage}</Route>
-        <Route path="/">{SearchPage}</Route>
-      </Switch>
-    </Container>
-  </Router>
-);
+const App = () => {
+  useEffect(() => {
+    // Run once, get the large index file into IndexDB + also check for index updates
+  }, []);
+  return (
+    <Router>
+      <Container className="p-3">
+        <Navbar bg="light" expand="lg">
+          <Link className="navbar-brand" to="/">
+            <h3>Chedex</h3>
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              <Nav.Link
+                target="_blank"
+                href="https://github.com/h4ck4life/chedex_react"
+              >
+                Github
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/">
+            <SearchPage />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FormControl,
   ListGroup,
@@ -6,26 +6,75 @@ import {
   Pagination,
   Form
 } from "react-bootstrap";
+import Elasticlunr from "elasticlunr";
 
 import Post from "../components/Post";
 
-const listItems = function () {
-  return Array(10)
-    .fill()
+const getSearchIndex = function name() {
+  let searchResults = [
+    {
+      id: 1,
+      title: "ikan",
+      content: "ikan bilis, ikan goreng",
+      date: "12 August 2016"
+    },
+    {
+      id: 2,
+      title: "daging",
+      content: "kambing, lembu",
+      date: "7 Jan 2011"
+    },
+    {
+      id: 3,
+      title: "ayam",
+      content: "kampung, kfc, mcd, kentang goreng",
+      date: "15 July 2018"
+    },
+    {
+      id: 4,
+      title: "kentang",
+      content: "frend fries, kentang goreng",
+      date: "3 March 2020"
+    }
+  ];
+
+  var index = Elasticlunr(function () {
+    this.addField("title");
+    this.addField("content");
+    this.addField("date");
+    this.setRef("id");
+    this.saveDocument(true);
+  });
+
+  searchResults.forEach((val, idx) => {
+    index.addDoc(val);
+  });
+
+  return index;
+};
+
+const ListItems = function (props) {
+  return getSearchIndex()
+    .search(props.keyword, {
+      fields: {
+        title: { boost: 1 },
+        content: { boost: 2 }
+      },
+      bool: "AND"
+    })
     .map((_, i) => {
+      let item = getSearchIndex().documentStore.getDoc(_.ref);
       return (
         <ListGroup.Item key={i}>
-          <Post
-            title="Title of the post"
-            content="Almost before we knew it, we had left the ground.Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground. Almost before we knew it, we had left the ground."
-            date="12 May 2018"
-          />
+          <Post title={item.title} content={item.content} date={item.date} />
         </ListGroup.Item>
       );
     });
 };
 
-const paginationBasic = function () {
+const SearchView = function (props) {
+  const [keyword, setKeyword] = useState();
+
   let active = 2;
   let items = [];
   for (let number = 1; number <= 3; number++) {
@@ -40,49 +89,48 @@ const paginationBasic = function () {
     );
   }
   return (
-    <div className="w-100 d-inline-flex mt-4 mb-3">
-      <div className="d-flex flex-fill align-items-baseline">
-        <Form
-          inline
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log("alif");
-          }}
-        >
-          <FormControl
-            size="md"
-            type="text"
-            placeholder="Search here.."
-            className=""
-            onChange={(e) => {
-              if (e.target.value.length > 3 || e.target.value.trim() !== "") {
-                console.log(e.target.value);
-              }
+    <div>
+      <div className="w-100 d-inline-flex mt-4 mb-3">
+        <div className="d-flex flex-fill align-items-baseline">
+          <Form
+            inline
+            onSubmit={(e) => {
+              e.preventDefault();
             }}
-          />
-          {/* <Button className="btnSearch" variant="primary" size="lg">
-          Search
-        </Button> */}
-        </Form>
-        {/* <Spinner
-          variant="secondary"
-          animation="border"
-          size="sm"
-          role="status"
-        /> */}
+          >
+            <FormControl
+              value={keyword || ""}
+              size="md"
+              type="text"
+              placeholder="Search here.."
+              className=""
+              onChange={(e) => {
+                setKeyword(e.target.value);
+                if (e.target.value.length > 3 && e.target.value.trim() !== "") {
+                  // do the search here
+                  console.log(e.target.value);
+                }
+              }}
+            />
+          </Form>
+        </div>
+        <div className="d-flex flex-fill flex-row-reverse">
+          <Pagination className="d-flex align-items-baseline mb-0">
+            {items}
+          </Pagination>
+        </div>
       </div>
-      <div className="d-flex flex-fill flex-row-reverse">
-        <Pagination className="d-flex align-items-baseline mb-0">
-          {items}
-        </Pagination>
-      </div>
+      <ListGroup>
+        <ListItems keyword={keyword} />
+      </ListGroup>
     </div>
   );
 };
 
-export default (props) => (
-  <div>
-    {paginationBasic()}
-    <ListGroup>{listItems()}</ListGroup>
-  </div>
-);
+export default () => {
+  return (
+    <div>
+      <SearchView />
+    </div>
+  );
+};
