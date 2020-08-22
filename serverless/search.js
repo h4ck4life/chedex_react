1bd393d32c8651cfc88243d81fed3ab37e272516
Alif <alifaziz@gmail.com>
2020-08-22 13:56:55 +0800

    Remove functions

--- serverless/search.js
+++
@@ -1,8 +0,0 @@
-
-exports.handler = function (event, context, callback) {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
-    }
-}
