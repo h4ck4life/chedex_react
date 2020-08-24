8936da28663ed1911fae416f851cf9f150d1aa0c
Alif <alifaziz@gmail.com>
2020-08-24 15:21:58 +0800

    add about page content

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,25 +6,27 @@ export default (props) => (
     <Row className="mb-3">
       <Col>
         <h5>What is Chedex</h5>
-        <p>lorem ipsum lorem ipsum lorem ipsum</p>
+        <p>Chedex is a full-text search webapp of Tun Mahathir's official blog posts, <a href="http://chedet.cc/">chedet.cc</a>. You're able to search chedet blog posts by using single/mutiple keyword. The results is sorted by rationality ranking/scoring <a href="https://github.com/weixsong/elasticlunr.js">(read more)</a></p>
       </Col>
     </Row>
     <Row className="mb-3">
       <Col>
-        <h5>Why we build this</h5>
-        <p>lorem ipsum lorem ipsum lorem ipsum</p>
-      </Col>
-    </Row>
-    <Row className="mb-3">
-      <Col>
-        <h5>How does it work</h5>
-        <p>lorem ipsum lorem ipsum lorem ipsum</p>
+        <h5>Source code</h5>
+        <p>You're welcome to contribute to make this webapp better,</p>
+        <p>
+          <div>Scraper + Indexer → <a rel="noopener noreferrer" target="_blank" href="https://github.com/h4ck4life/chedex">https://github.com/h4ck4life/chedex</a></div>
+          <div>Web App → <a rel="noopener noreferrer" target="_blank" href="https://github.com/h4ck4life/chedex_react">https://github.com/h4ck4life/chedex_react</a></div>
+        </p>
       </Col>
     </Row>
     <Row className="mb-3">
       <Col>
         <h5>Contact</h5>
-        <p>lorem ipsum lorem ipsum lorem ipsum</p>
+        <p>
+          <div>Email → <a href="mailto:alifaziz@gmail.com">alifaziz@gmail.com</a></div>
+          <div>Github → <a rel="noopener noreferrer" target="_blank" href="https://github.com/h4ck4life">h4ck4life</a></div>
+          <div>Twitter → <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/h4ck4life">@h4ck4life</a></div>
+        </p>
       </Col>
     </Row>
   </Container>
