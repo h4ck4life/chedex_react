1c229736aa70ab396d13af62451276dc4337e7a0
Alif <alifaziz@gmail.com>
2021-05-01 01:53:29 +0800

    add share buttons

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,31 +2,45 @@
  * Regex to find para - https://regex101.com/r/XtfS4Y/1
  */
 
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Badge,
 } from "react-bootstrap";
+import Sharect from "sharect";
+
 
 let createMarkup = function (content, keyword) {
   return { __html: content };
 };
 
-export default (props) => (
-  <div className="pt-3 pb-3">
-    <div className="mb-3">
-      <div className="d-flex align-items-baseline w-100">
-        <div className="d-flex flex-fill align-items-baseline">
-          <b>{props.title}</b>
+export default (props) => {
+
+  Sharect.config({
+    facebook: true,
+    twitter: true,
+    //twitterUsername: 'estevanmaito',
+    backgroundColor: '#007bff',
+    iconColor: '#FFF',
+    selectableElements: ['.post'],
+  }).init();
+
+  return (
+    <div className="pt-3 pb-3">
+      <div className="mb-3">
+        <div className="d-flex align-items-baseline w-100">
+          <div className="d-flex flex-fill align-items-baseline">
+            <b>{props.title}</b>
+          </div>
+          <div className="d-flex flex-fill flex-row-reverse">
+            <a target="_blank" className="originalPost" rel="noopener noreferrer" title={props.url} href={props.url}>View original post</a>
+          </div>
         </div>
-        <div className="d-flex flex-fill flex-row-reverse">
-          <a target="_blank" className="originalPost" rel="noopener noreferrer" title={props.url} href={props.url}>View original post</a>
+        <div>
+          <Badge className="mr-1 score" title="Rationality scoring" variant="light">score {(props.score).toFixed(1) || 0}</Badge>
+          <span className="postDate">{props.date.split(' | ')[0]}</span>
         </div>
       </div>
-      <div>
-        <Badge className="mr-1 score" title="Rationality scoring" variant="light">score {(props.score).toFixed(1) || 0}</Badge>
-        <span className="postDate">{props.date.split(' | ')[0]}</span>
-      </div>
+      <div className="post" dangerouslySetInnerHTML={createMarkup(props.content, props.keyword)} />
     </div>
-    <div className="post" dangerouslySetInnerHTML={createMarkup(props.content, props.keyword)} />
-  </div>
-);
+  )
+};
